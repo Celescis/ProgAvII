b13de8128e2923854b2eac8fc6fb7f6307238fca
Celeste <celestecisternas@gmail.com>
2024-07-08 01:45:49 -0300

    [update] notificaciones

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,7 @@ const authController = {
       if (!user) {
         req.flash('error', 'Usuario no encontrado');
         res.locals.errorMessage = 'Usuario no encontrado';
-        return res.redirect('/auth/login');
+        return res.redirect(`/auth/login?message=${encodeURIComponent('noexiste')}`);
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
@@ -19,13 +19,13 @@ const authController = {
       if (!isMatch) {
         req.flash('error', 'Contraseña incorrecta');
         res.locals.errorMessage = 'Contraseña incorrecta';
-        return res.redirect('/auth/login');
+        return res.redirect(`/auth/login?message=${encodeURIComponent('passmal')}`);
       }
 
       req.session.userId = user._id;
       req.flash('success', 'Login exitoso');
       res.locals.successMessage = 'Login exitoso';
-      res.redirect('/');
+      return res.redirect(`/?message=${encodeURIComponent('login')}`);
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
       req.flash('error', 'Error interno del servidor');
@@ -37,6 +37,13 @@ const authController = {
   registerUser: async (req, res) => {
     try {
       const { username, password } = req.body;
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        req.flash('error', 'El nombre de usuario ya está en uso');
+        res.locals.errorMessage = 'El nombre de usuario ya está en uso';
+        return res.redirect(`/auth/register?message=${encodeURIComponent('repite')}`);
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const newUser = new User({
@@ -47,7 +54,7 @@ const authController = {
       await newUser.save();
       req.flash('success', 'Registro exitoso');
       res.locals.successMessage = 'Registro exitoso';
-      res.redirect('/auth/login');
+      return res.redirect(`/auth/login?message=${encodeURIComponent('registro')}`);
     } catch (error) {
       console.error('Error al registrar usuario:', error);
       req.flash('error', 'Error interno del servidor');
@@ -64,7 +71,7 @@ const authController = {
         res.locals.errorMessage = 'Error interno del servidor';
         res.redirect('/');
       } else {
-        res.redirect('/auth/login');
+        return res.redirect(`/auth/login?message=${encodeURIComponent('cierra')}`);
       }
     });
   }
